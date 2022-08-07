0b60b96f9a00285ecec2d240dcd980c43e73792a
Виктор <Vik2697@yandex.ru>
2022-08-07 12:45:19 +0300

    First try

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -14,10 +14,10 @@ public class User {
     @Email(message = "Введённое значение не является почтой")
     private String email;
     @NotBlank(message = "Логин не должен быть пустым")
-    @Pattern(regexp = "^[0-9a-zA-Zа-яА-Я]$")
+    @Pattern(regexp = "^[0-9a-zA-Zа-яА-Я]$",message = "Логин не должен содержать пробелов")
     private String login;
     private String name;
-    @Past
+    @Past(message = "День рождения пользователя не должен быть в будущем")
     private Long birthday;
 
     public User(String email, String login, String name, Long birthday) {
